963f9818796dcb086a9f003e9377467de75130ec
JLongarzo <57125141+JLongarzo@users.noreply.github.com>
2020-02-24 00:15:38 -0500

    response, error checking, parse data, print
    added functionality for handling the response, parsed the data and put it into a set of arrays. Printed necessary data for 52 week high/lows

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -54,3 +54,64 @@ print("-------------------------")
 
 url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={userInput}&outputsize=full&apikey={apiKey}"
 
+
+
+response = requests.get(url)
+
+#check for error
+if "Error" in response.text:
+    print("Sorry, couldn't find any trading data for that stock symbol, please try again!")
+    exit()
+
+
+parsedResponse = response.json()
+
+
+
+
+
+dates = []
+openPrices = []
+highPrices = []
+lowPrices = []
+closePrices = []
+dailyVolumes = []
+
+z = 0
+for x, y in parsedResponse['Time Series (Daily)'].items():
+    if z < 253:
+        dates.append(x)
+        openPrices.append(float(y['1. open']))
+        highPrices.append(float(y['2. high']))
+        lowPrices.append(float(y['3. low']))
+        closePrices.append(float(y['4. close']))
+        dailyVolumes.append(y['5. volume'])
+        z = z + 1
+
+
+
+
+latestDate = dates[0]
+
+latestClose = to_usd(closePrices[0])
+
+annualHigh = to_usd(float(max(highPrices)))
+
+annualLow = to_usd(float(min(lowPrices)))
+
+
+
+print(f"MOST RECENT TRADING DAY: {latestDate}")
+print(f"LATEST CLOSE: {latestClose}")
+print(f"52 WEEK HIGH: {annualHigh}")
+print(f"52 WEEK LOW: {annualLow}")
+print("-------------------------")
+
+
+
+
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
